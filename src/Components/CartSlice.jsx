27bb6841fb69770b6ebdba3594f3326914568a8a
adnrussg <russelgarcia1551@gmail.com>
2024-09-17 13:17:44 -0400

    Modified CartSlice as initialState was not read properly and included a supercoin functionality

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,31 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+const initialState = {
+    cartItems: [],
+};
 const CartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
         addItemToCart(state, action) {
-            const existingItem = state.cartitems.find(item => item.id === action.payload.id);
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id);
             if (existingItem) {
                 existingItem.quantity += 1;
             } else {
-                state.cartitems.push({ ...action.payload, quantity: 1});
+                state.cartItems.push({ ...action.payload, quantity: 1});
             }
         },
         removeItemFromCart(state, action) {
-            state.cartitems = state.cartitems.filter(item => item.id !== action.payload);
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
         },
         clearCart(state) {
-            state.cartitems = [];
+            state.cartItems = [];
         },
         increaseItemQuantity(state, action) {
-            const itemToIncrease = state.cartitems.find(item => item.id === action.payload);
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
             if (itemToIncrease) {
                 itemToIncrease.quantity += 1;
             }
         },
         decreaseItemQuantity(state, action) {
-            const itemToDecrease = state.cartitems.find(item => item.id === action.payload);
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
             if (itemToDecrease && itemToDecrease.quantity > 1) {
                 itemToDecrease.quantity -= 1;
             }
@@ -41,7 +43,3 @@ export const {
     decreaseItemQuantity,
 } = CartSlice.actions;
 export default CartSlice.reducer;
-
-const initialState = {
-    cartitems: [],
-};
