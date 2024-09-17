3b0d06d32ae6865c042db3113d40a743854e8c61
adnrussg <russelgarcia1551@gmail.com>
2024-09-17 12:39:25 -0400

    E-Commerce Data Rendering using React Toolkit

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit';
 
-const CartSlice = ({
- 
+const CartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItemToCart(state, action) {
+            const existingItem = state.cartitems.find(item => item.id === action.payload.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.cartitems.push({ ...action.payload, quantity: 1});
+            }
+        },
+        removeItemFromCart(state, action) {
+            state.cartitems = state.cartitems.filter(item => item.id !== action.payload);
+        },
+        clearCart(state) {
+            state.cartitems = [];
+        },
+        increaseItemQuantity(state, action) {
+            const itemToIncrease = state.cartitems.find(item => item.id === action.payload);
+            if (itemToIncrease) {
+                itemToIncrease.quantity += 1;
+            }
+        },
+        decreaseItemQuantity(state, action) {
+            const itemToDecrease = state.cartitems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+                itemToDecrease.quantity -= 1;
+            }
+        },
+    }
 });
 
+export const {
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+} = CartSlice.actions;
+export default CartSlice.reducer;
 
+const initialState = {
+    cartitems: [],
+};
